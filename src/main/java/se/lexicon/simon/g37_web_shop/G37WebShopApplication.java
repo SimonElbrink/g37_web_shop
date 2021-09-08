f83931cfe210da1ff1f8560db7307c073d623986
Simon Elbrink <simon.elbrink@gmail.com>
2021-09-08 10:33:32 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/simon/g37_web_shop/G37WebShopApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.simon.g37_web_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G37WebShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G37WebShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/simon/g37_web_shop/G37WebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.simon.g37_web_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G37WebShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
